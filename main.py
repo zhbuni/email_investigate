385da54d40eee1dd39b8dccf62578450e5ff1133
Vladimir <xdf211@gmail.com>
2022-02-24 20:34:48 +0300

    commit

--- main.py
+++ main.py
@@ -39,6 +39,10 @@ list_of_answers = [
     'Пожалуйста, задавайте вопросы по делу, ничего не понятно...']
 
 
+suspects = ['Алина Кукушкина', 'Ирина Фокина', 'Светлана Комарова', 'Павел Старков',
+            'Глеб Горбунов', 'Сергей Фролов', 'Марлен Кветенадзе', 'Наталья Ткачева', 'Мария Мухина']
+
+
 def send_mail(to, answer, subject):
     smtp = SMTP(smtp_server, detective_login, detective_password, detective_mail)
     smtp.send_mail(to, 'RE:{}'.format(subject), answer)
@@ -123,7 +127,9 @@ def main():
 
             for word in words:
                 p = morph.parse(word)[0]
-                normal_words.append(p.normal_form.upper())
+                if p.normal_form.upper():
+                    normal_words.append(p.normal_form.upper())
+
             # if 'КУКУШКИН' in normal_words:
             #     normal_words.append('КУКУШКИНА')
             del words
@@ -139,63 +145,76 @@ def main():
             item_of_norm = None
             items_with_flag = []
             print(set(hard_words), set(normal_words))
-            if len(set(hard_words).intersection(set(normal_words))) == 1:
-                item_of_norm = set(hard_words).intersection(set(normal_words)).pop()
-                flag = True if item_of_norm in normal_words else False
-                print(episode == 'АФ032020 БАКУ')
-                lst = [el for el in items if el[1] == episode]
-                items_with_flag = [el for el in items if item_of_norm.lower()
-                                   in str(el[0]).lower() and el[1] == episode]
-            for n_word in normal_words:
-                items_to_iterate = items_with_flag if flag else [el for el in items if el[1] == episode]
-                print(items_to_iterate)
-                break_normal = False
-                if break_normal:
-                    break
-                for el in items_to_iterate:
-                    if not flag:
-                        inter = len(set(hard_words).intersection(set([el1.upper() for el1 in str(el[0]).split()])))
-                        if inter:
-                            continue
-                    if item:
-                        break_normal = True
-                        break
-                    test_word = ''
-                    splitted_item = []
-                    for el1 in str(el[0]).split():
-                        p = morph.parse(el1)[0]
-                        splitted_item.append(p.normal_form.upper())
-                    for word in splitted_item:
-                        if n_word == word:
-                            test_word = n_word
-                    if not test_word:
-                        continue
-                    is_break = False
-                    if len(splitted_item) == 1:
+            new_suspects = set()
+            normal_suspects = []
+            for word in suspects:
+                w1, w2 = word.split()
+                p1 = morph.parse(w1)[0].normal_form.upper()
+                p2 = morph.parse(w2)[0].normal_form.upper()
+                new_suspects.add(f'{p1} {p2}')
+                normal_suspects.append(f'{p1} {p2}')
+            current_suspects = []
+            for word in normal_words:
+                for el in normal_suspects:
+                    lst = [_.upper() for _ in el.split()]
+                    susp = ' '.join(lst)
+                    if word in lst and susp in new_suspects:
+                        new_suspects.remove(susp)
+                        current_suspects.append(susp)
+            if len(current_suspects) == 2:
+                for el in items:
+                    low = el[0].lower()
+                    if current_suspects[0].lower() in low and current_suspects[1].lower() in low and el[1] == episode:
                         item = el[0]
-                        is_break = True
                         break
-                    else:
+            else:
+                if len(set(hard_words).intersection(set(normal_words))) == 1:
+                    item_of_norm = set(hard_words).intersection(set(normal_words)).pop()
+                    flag = True if item_of_norm in normal_words else False
+                    print(episode == 'АФ032020 БАКУ')
+                    lst = [el for el in items if el[1] == episode]
+                    items_with_flag = [el for el in items if item_of_norm.lower()
+                                       in str(el[0]).lower() and el[1] == episode]
+                for n_word in normal_words:
+                    items_to_iterate = items_with_flag if flag else [el for el in items if el[1] == episode]
+                    print(items_to_iterate)
+                    break_normal = False
+                    if break_normal:
+                        break
+                    for el in items_to_iterate:
                         if not flag:
+                            inter = len(set(hard_words).intersection(set([el1.upper() for el1 in str(el[0]).split()])))
+                            if inter:
+                                continue
+                        if item:
+                            break_normal = True
+                            break
+                        test_word = ''
+                        splitted_item = []
+                        for el1 in str(el[0]).split():
+                            p = morph.parse(el1)[0]
+                            splitted_item.append(p.normal_form.upper())
+                        for word in splitted_item:
+                            if n_word == word:
+                                test_word = n_word
+                        if not test_word:
+                            continue
+                        is_break = False
+                        if len(splitted_item) == 1:
                             item = el[0]
+                            is_break = True
+                            break
                         else:
-                            for word in normal_words:
-                                if word in splitted_item and word != test_word and not item:
-                                    item = el[0]
-                                    is_break = True
-                                    break
-                    if is_break:
-                        break
-                    if n_word.startswith('АФ'):
-                        episode = n_word
-                    if n_word in first_names:
-                        sus_f_name.append(n_word)
-                    if n_word in last_names:
-                        sus_l_name.append(n_word)
-                    # if n_word in items:
-                    #     item = n_word
-                if n_word == 'ПОДСКАЗКА' or n_word == "ОТВЕТ":
-                    action = n_word
+                            if not flag:
+                                item = el[0]
+                            else:
+                                for word in normal_words:
+                                    if word in splitted_item and word != test_word and not item:
+                                        item = el[0]
+                                        is_break = True
+                                        break
+                        if is_break:
+                            break
             print(f'item is {item}')
             if not action:
                 action = 'ПОДСКАЗКА'
